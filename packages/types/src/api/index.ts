4ad527de8127fe488eff83710cfbb09e23814c00
Benno <57860196+bennoinbeta@users.noreply.github.com>
2025-06-13 16:46:36 +0200

    #3 fixed typos

--- packages/types/src/api/index.ts
+++ packages/types/src/api/index.ts
@@ -3,4 +3,3 @@ import { type components } from './types/core-api-v1.gen';
 export * as coreApiV1 from './types/core-api-v1.gen';
 
 export type TAppErrorDto = components['schemas']['AppErrorDto'];
-export type TMetatagsDto = components['schemas']['MetatagsDto'];
