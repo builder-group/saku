713ce1aa4601568dd08efce37b311c099efadb6b
Benno <57860196+bennoinbeta@users.noreply.github.com>
2025-07-23 09:17:10 +0200

    #17 fixed mobile preview view

--- apps/sfy-link-in-bio/app/src/features/page-editor/components/StaticCanvasPanel/StaticCanvasPanel.tsx
+++ apps/sfy-link-in-bio/app/src/features/page-editor/components/StaticCanvasPanel/StaticCanvasPanel.tsx
@@ -2,6 +2,7 @@ import { Spinner } from '@shopify/polaris';
 import { useFeatureState } from 'feature-react';
 import React from 'react';
 import { ResizablePanel, ShadowRoot } from '@/components';
+import { cn } from '@/lib';
 import tailwindStylesHref from '@/styles.css?url';
 import { EditorSiteResolveContext, resolvePageNode, TPageEditor } from '../../lib';
 import { StaticNodeCanvas } from '../NodeCanvas';
@@ -38,16 +39,26 @@ export const StaticCanvasPanel: React.FC<TStaticCanvasPanelProps> = (props) => {
 
 			<div
 				ref={editor.canvasContainerRef}
-				className={`h-[calc(100%-3rem)] w-full overflow-y-auto bg-neutral-50 ${
-					viewMode === 'mobile' ? 'flex justify-center' : ''
-				}`}
+				className={cn(
+					'h-[calc(100%-3rem)] w-full overflow-y-auto',
+					viewMode === 'mobile' && 'flex justify-center'
+				)}
+				style={{
+					backgroundImage:
+						'repeating-linear-gradient(-45deg, var(--color-neutral-50), var(--color-neutral-50) 13px, var(--color-neutral-200) 13px, var(--color-neutral-200) 14px)',
+					backgroundSize: '40px 40px'
+				}}
 			>
 				{/* Use ShadowRoot to fully isolate the static canvas from global styles (e.g., Polaris), ensuring only Tailwind styles apply inside. */}
 				<ShadowRoot
 					links={[{ rel: 'stylesheet', href: tailwindStylesHref }]}
 					onStylesLoaded={() => setStylesLoaded(true)}
 				>
-					<div className={viewMode === 'mobile' ? 'w-[390px]' : 'w-full'}>
+					<div
+						className={cn(
+							viewMode === 'mobile' ? 'w-[390px] border-r border-l border-black' : 'w-full'
+						)}
+					>
 						<StaticNodeCanvas cx={editor.pageContext} nodes={[rootNode]} />
 					</div>
 				</ShadowRoot>
