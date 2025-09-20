2aa929c9d46a65ac7a43f5f0007874cc16c0e9f5
Benno <57860196+bennoinbeta@users.noreply.github.com>
2025-09-20 16:11:32 +0200

    #46 fixed unreference ref type

--- packages/editor/src/types/ref.ts
+++ packages/editor/src/types/ref.ts
@@ -2,10 +2,10 @@ import { TTokenRef } from './token';
 
 export type TRef<T> = TTokenRef | T;
 
-export type TUnreference<T> = T extends { type: 'token' }
+export type TUnreference<T> = T extends TTokenRef
 	? never // Remove token completely
 	: T extends object
 		? { [K in keyof T]: TUnreference<T[K]> }
 		: T;
 
-export type TUnreferenceTop<T> = T extends { type: 'token' } ? never : T;
+export type TUnreferenceTop<T> = T extends TTokenRef ? never : T;
