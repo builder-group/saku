23888d4e740faf6d7e4db81b4611b4fd3a991986
Benno <57860196+bennoinbeta@users.noreply.github.com>
2025-09-24 07:32:40 +0200

    #46 wip

--- apps/sfy-link-in-bio/app/src/features/page-editor/.server/lib/hydrate/site-hydrate-context.ts
+++ apps/sfy-link-in-bio/app/src/features/page-editor/.server/lib/hydrate/site-hydrate-context.ts
@@ -5,46 +5,20 @@ import {
 	TFlatSite,
 	TIntegration,
 	TIntegrationId,
-	TMixinTokenGroupMap,
 	TNodeId,
-	TVariableToken
+	TToken
 } from '@repo/editor';
 import { TSiteHydrateContext } from './types';
 
-/**
- * Site-based provider - works with static site data
- */
 export class StaticSiteHydrateContext implements TSiteHydrateContext {
 	public readonly id: string;
 	public readonly handle: string;
 	private readonly site: TFlatSite;
-	private readonly mixinTokenGroupMap: TMixinTokenGroupMap = {};
-	private readonly variableTokenMap: Record<string, TVariableToken> = {};
 
 	constructor(site: TFlatSite, id: string, handle: string) {
 		this.site = site;
 		this.id = id;
 		this.handle = handle;
-		this.createTokenMaps();
-	}
-
-	private createTokenMaps(): void {
-		Object.values(this.site.tokens).forEach((token) => {
-			switch (token.type) {
-				case 'mixin': {
-					const mixinKey = token.mixinKey;
-					if (this.mixinTokenGroupMap[mixinKey] == null) {
-						this.mixinTokenGroupMap[mixinKey] = {};
-					}
-					this.mixinTokenGroupMap[mixinKey][token.key] = token;
-					break;
-				}
-				case 'variable': {
-					this.variableTokenMap[token.key] = token;
-					break;
-				}
-			}
-		});
 	}
 
 	public getNode(id: TNodeId): TFlatNode | null {
@@ -63,13 +37,7 @@ export class StaticSiteHydrateContext implements TSiteHydrateContext {
 		return this.site.integrations[id] ?? null;
 	}
 
-	public getMixinTokenSet<GGroupKey extends keyof TMixinTokenGroupMap>(
-		groupKey: GGroupKey
-	): TMixinTokenGroupMap[GGroupKey] | null {
-		return this.mixinTokenGroupMap[groupKey] || null;
-	}
-
-	public getVariableTokenMap(): Record<string, TVariableToken> {
-		return this.variableTokenMap;
+	public getTokenMap(): Record<string, TToken> {
+		return this.site.tokens;
 	}
 }
--- apps/sfy-link-in-bio/app/src/features/page-editor/nodes/link-node/resolve-node.ts
+++ apps/sfy-link-in-bio/app/src/features/page-editor/nodes/link-node/resolve-node.ts
@@ -76,9 +76,7 @@ export function resolveLinkNode(
 	const [isResolvedAutoLayoutOk, resolvedAutoLayoutErr, resolvedAutoLayout] =
 		resolveAutoLayoutStyleMixin(autoLayout, {
 			node: cx,
-			mixinTokenSet: cx.site.getMixinTokenSet('autoLayout'),
-			mapToMixinTokenValue: (ref, tokenSet) => tokenSet?.[ref]?.value,
-			variableTokenMap: cx.site.getVariableTokenMap()
+			tokenMap: cx.site.getTokenMap()
 		});
 	if (!isResolvedAutoLayoutOk) {
 		return Err(resolvedAutoLayoutErr.wrapWith('#ERR_RESOLVE_AUTO_LAYOUT_STYLE'));
@@ -86,63 +84,49 @@ export function resolveLinkNode(
 	const [isResolvedAppearanceOk, resolvedAppearanceErr, resolvedAppearance] =
 		resolveAppearanceStyleMixin(appearance, {
 			node: cx,
-			mixinTokenSet: cx.site.getMixinTokenSet('appearance'),
-			mapToMixinTokenValue: (ref, tokenSet) => tokenSet?.[ref]?.value,
-			variableTokenMap: cx.site.getVariableTokenMap()
+			tokenMap: cx.site.getTokenMap()
 		});
 	if (!isResolvedAppearanceOk) {
 		return Err(resolvedAppearanceErr.wrapWith('#ERR_RESOLVE_APPEARANCE_STYLE'));
 	}
 	const [isResolvedFillOk, resolvedFillErr, resolvedFill] = resolveFillStyleMixin(fill, {
 		node: cx,
-		mixinTokenSet: cx.site.getMixinTokenSet('fill'),
-		mapToMixinTokenValue: (ref, tokenSet) => tokenSet?.[ref]?.value,
-		variableTokenMap: cx.site.getVariableTokenMap()
+		tokenMap: cx.site.getTokenMap()
 	});
 	if (!isResolvedFillOk) {
 		return Err(resolvedFillErr.wrapWith('#ERR_RESOLVE_FILL_STYLE'));
 	}
 	const [isResolvedStrokeOk, resolvedStrokeErr, resolvedStroke] = resolveStrokeStyleMixin(stroke, {
 		node: cx,
-		mixinTokenSet: cx.site.getMixinTokenSet('stroke'),
-		mapToMixinTokenValue: (ref, tokenSet) => tokenSet?.[ref]?.value,
-		variableTokenMap: cx.site.getVariableTokenMap()
+		tokenMap: cx.site.getTokenMap()
 	});
 	if (!isResolvedStrokeOk) {
 		return Err(resolvedStrokeErr.wrapWith('#ERR_RESOLVE_STROKE_STYLE'));
 	}
 	const [isResolvedShadowOk, resolvedShadowErr, resolvedShadow] = resolveShadowStyleMixin(shadow, {
 		node: cx,
-		mixinTokenSet: cx.site.getMixinTokenSet('shadow'),
-		mapToMixinTokenValue: (ref, tokenSet) => tokenSet?.[ref]?.value,
-		variableTokenMap: cx.site.getVariableTokenMap()
+		tokenMap: cx.site.getTokenMap()
 	});
 	if (!isResolvedShadowOk) {
 		return Err(resolvedShadowErr.wrapWith('#ERR_RESOLVE_SHADOW_STYLE'));
 	}
 	const [isResolvedTextOk, resolvedTextErr, resolvedText] = resolveTextStyleMixin(text, {
 		node: cx,
-		mixinTokenSet: cx.site.getMixinTokenSet('text'),
-		mapToMixinTokenValue: (ref, tokenSet) => tokenSet?.[ref]?.value,
-		variableTokenMap: cx.site.getVariableTokenMap()
+		tokenMap: cx.site.getTokenMap()
 	});
 	if (!isResolvedTextOk) {
 		return Err(resolvedTextErr.wrapWith('#ERR_RESOLVE_TEXT_STYLE'));
 	}
 	const [isResolvedSmTextOk, resolvedSmTextErr, resolvedSmText] = resolveTextStyleMixin(textSm, {
 		node: cx,
-		mixinTokenSet: cx.site.getMixinTokenSet('text'),
-		mapToMixinTokenValue: (ref, tokenSet) => tokenSet?.[ref]?.value,
-		variableTokenMap: cx.site.getVariableTokenMap()
+		tokenMap: cx.site.getTokenMap()
 	});
 	if (!isResolvedSmTextOk) {
 		return Err(resolvedSmTextErr.wrapWith('#ERR_RESOLVE_SM_TEXT_STYLE'));
 	}
 	const [isResolvedImageOk, resolvedImageErr, resolvedImage] = resolveImageStyleMixin(image, {
 		node: cx,
-		mixinTokenSet: cx.site.getMixinTokenSet('image'),
-		mapToMixinTokenValue: (ref, tokenSet) => tokenSet?.[ref]?.value,
-		variableTokenMap: cx.site.getVariableTokenMap()
+		tokenMap: cx.site.getTokenMap()
 	});
 	if (!isResolvedImageOk) {
 		return Err(resolvedImageErr.wrapWith('#ERR_RESOLVE_IMAGE_STYLE'));
