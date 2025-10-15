89f3b86fe3928008a08d12f951dd1b3a2d3ad103
Benno <57860196+bennoinbeta@users.noreply.github.com>
2025-10-15 20:38:54 +0200

    #develop fixed jumping cursor in about node

--- apps/sfy-link-in-bio/app/src/hooks/use-map-state.ts
+++ apps/sfy-link-in-bio/app/src/hooks/use-map-state.ts
@@ -26,7 +26,7 @@ export function mapState<GBaseValue, GMappedValue>(
 ): [TState<GMappedValue, any>, () => void] {
 	const { map, sync, isEqual = Object.is, ...stateOptions } = config;
 
-	const mappedState = createState(map(baseState._v), stateOptions);
+	const mappedState = createState(map(baseState._v), { queue: 'sync', ...stateOptions });
 
 	// Keep mapped in sync with base
 	const unsubscribeBaseState = baseState.listen(({ value, source }) => {
