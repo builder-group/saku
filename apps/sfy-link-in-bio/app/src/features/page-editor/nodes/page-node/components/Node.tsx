8fe1c17343da08fc9b94dafa1635f3a6c5638bd5
Benno <57860196+bennoinbeta@users.noreply.github.com>
2025-10-16 10:56:01 +0200

    #develop fixed typos

--- apps/sfy-link-in-bio/app/src/features/page-editor/nodes/page-node/components/Node.tsx
+++ apps/sfy-link-in-bio/app/src/features/page-editor/nodes/page-node/components/Node.tsx
@@ -12,6 +12,18 @@ export const PageNode = React.forwardRef<HTMLDivElement, TNodeProps<TFlatPageNod
 	(props, ref) => {
 		const { nodeState, editor, ...divProps } = props;
 
+		const { contentRadius, glassBorderRadius, borderWidth, borderColor } = React.useMemo(() => {
+			const contentRadius = 8; // px
+			const borderWidth = 5; // px
+			const borderColor = '#ffffff52';
+			return {
+				contentRadius,
+				glassBorderRadius: contentRadius + borderWidth,
+				borderWidth,
+				borderColor
+			};
+		}, []);
+
 		const node = useCombinedCompute([nodeState, editor.tokenMap], ([{ value: nodeValue }]) => {
 			const result = resolvePageNodeWithoutChildren(nodeValue, {
 				site: new EditorSiteResolveContext(editor)
@@ -50,21 +62,41 @@ export const PageNode = React.forwardRef<HTMLDivElement, TNodeProps<TFlatPageNod
 						...fill?.styles
 					}}
 				>
-					<div className="absolute inset-0 hidden bg-black/10 sm:block" />
+					<div className="absolute inset-0 hidden bg-black/10 backdrop-blur-xl sm:block" />
 
 					<div className="relative mx-auto w-full max-w-xl sm:pt-10">
-						<div
-							className="overflow-hidden sm:rounded-t-lg sm:pb-10 sm:shadow-[0_24px_32px_0px_rgba(0,0,0,0.15)]"
-							style={{
-								...appearance.styles,
-								...fill?.styles
-							}}
-						>
-							<div className="flex w-full flex-col" style={autoLayout.styles}>
-								{childNodes.map((childNodeState) => (
-									<Node key={childNodeState._v.id} nodeState={childNodeState} editor={editor} />
-								))}
+						<div className="relative">
+							<div
+								className="overflow-hidden sm:pb-10 sm:shadow-[0_24px_32px_0px_rgba(0,0,0,0.15)]"
+								style={{
+									...appearance.styles,
+									...fill?.styles,
+									borderTopLeftRadius: contentRadius,
+									borderTopRightRadius: contentRadius
+								}}
+							>
+								<div className="flex w-full flex-col" style={autoLayout.styles}>
+									{childNodes.map((childNodeState) => (
+										<Node key={childNodeState._v.id} nodeState={childNodeState} editor={editor} />
+									))}
+								</div>
 							</div>
+
+							<div
+								className="pointer-events-none absolute top-0 left-0 hidden h-full w-full sm:block"
+								style={{
+									marginTop: -borderWidth,
+									marginLeft: -borderWidth,
+									width: `calc(100% + ${borderWidth * 2}px)`,
+									borderTopWidth: borderWidth,
+									borderRightWidth: borderWidth,
+									borderLeftWidth: borderWidth,
+									borderColor,
+									borderStyle: 'solid',
+									borderTopLeftRadius: glassBorderRadius,
+									borderTopRightRadius: glassBorderRadius
+								}}
+							/>
 						</div>
 					</div>
 				</div>
--- apps/sfy-link-in-bio/app/src/features/page-editor/nodes/page-node/components/ResolvedNode.tsx
+++ apps/sfy-link-in-bio/app/src/features/page-editor/nodes/page-node/components/ResolvedNode.tsx
@@ -11,6 +11,18 @@ export const ResolvedPageNode: React.FC<TResolvedNodeProps<TResolvedPageNode>> =
 		...divProps
 	} = props;
 
+	const { contentRadius, glassBorderRadius, borderWidth, borderColor } = React.useMemo(() => {
+		const contentRadius = 8; // px
+		const borderWidth = 5; // px
+		const borderColor = '#ffffff52';
+		return {
+			contentRadius,
+			glassBorderRadius: contentRadius + borderWidth,
+			borderWidth,
+			borderColor
+		};
+	}, []);
+
 	return (
 		<>
 			<div
@@ -21,21 +33,41 @@ export const ResolvedPageNode: React.FC<TResolvedNodeProps<TResolvedPageNode>> =
 					...fill?.styles
 				}}
 			>
-				<div className="absolute inset-0 hidden bg-black/10 sm:block" />
+				<div className="absolute inset-0 hidden bg-black/10 backdrop-blur-xl sm:block" />
 
-				<div className="relative mx-auto w-full max-w-md sm:pt-10">
-					<div
-						className="overflow-hidden sm:rounded-t-lg sm:pb-10 sm:shadow-[0_24px_32px_0px_rgba(0,0,0,0.15)]"
-						style={{
-							...appearance.styles,
-							...fill?.styles
-						}}
-					>
-						<div className="flex w-full flex-col" style={autoLayout.styles}>
-							{children.map((childNode) => (
-								<ResolvedNode key={childNode.id} node={childNode} cx={cx} />
-							))}
+				<div className="relative mx-auto w-full max-w-xl sm:pt-10">
+					<div className="relative">
+						<div
+							className="overflow-hidden sm:pb-10 sm:shadow-[0_24px_32px_0px_rgba(0,0,0,0.15)]"
+							style={{
+								...appearance.styles,
+								...fill?.styles,
+								borderTopLeftRadius: contentRadius,
+								borderTopRightRadius: contentRadius
+							}}
+						>
+							<div className="flex w-full flex-col" style={autoLayout.styles}>
+								{children.map((childNode) => (
+									<ResolvedNode key={childNode.id} node={childNode} cx={cx} />
+								))}
+							</div>
 						</div>
+
+						<div
+							className="pointer-events-none absolute top-0 left-0 hidden h-full w-full sm:block"
+							style={{
+								marginTop: -borderWidth,
+								marginLeft: -borderWidth,
+								width: `calc(100% + ${borderWidth * 2}px)`,
+								borderTopWidth: borderWidth,
+								borderRightWidth: borderWidth,
+								borderLeftWidth: borderWidth,
+								borderColor,
+								borderStyle: 'solid',
+								borderTopLeftRadius: glassBorderRadius,
+								borderTopRightRadius: glassBorderRadius
+							}}
+						/>
 					</div>
 				</div>
 			</div>
