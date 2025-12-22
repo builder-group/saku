556d36d8ad19e0feda0269959ba48e4104f0174e
Benno <57860196+bennoinbeta@users.noreply.github.com>
2025-12-22 10:37:15 +0100

    #66 fixed typos

--- apps/sfy-link-in-bio/app/src/components/layout/Resizable.tsx
+++ apps/sfy-link-in-bio/app/src/components/layout/Resizable.tsx
@@ -13,11 +13,18 @@
 'use client';
 
 import { Group, Panel, Separator } from 'react-resizable-panels';
+import { PolarisDragHandleIcon } from '@/components';
 import { cn } from '@/lib';
 
-const ResizablePanelGroup = ({ className, ...props }: React.ComponentProps<typeof Group>) => (
+const ResizablePanelGroup = ({
+	className,
+	orientation,
+	...props
+}: React.ComponentProps<typeof Group>) => (
 	<Group
-		className={cn('flex h-full w-full data-[orientation=vertical]:flex-col', className)}
+		className={cn('group flex h-full w-full data-[orientation=vertical]:flex-col', className)}
+		data-orientation={orientation}
+		orientation={orientation}
 		{...props}
 	/>
 );
@@ -33,17 +40,16 @@ const ResizableHandle = ({
 }) => (
 	<Separator
 		className={cn(
-			'bg-border focus-visible:ring-ring relative z-50 flex w-px items-center justify-center after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:outline-none data-[orientation=vertical]:h-px data-[orientation=vertical]:w-full data-[orientation=vertical]:after:left-0 data-[orientation=vertical]:after:h-1 data-[orientation=vertical]:after:w-full data-[orientation=vertical]:after:translate-x-0 data-[orientation=vertical]:after:-translate-y-1/2 [&[data-orientation=vertical]>div]:rotate-90',
+			'bg-border focus-visible:ring-ring relative z-50 flex w-px items-center justify-center group-data-[orientation=vertical]:h-px group-data-[orientation=vertical]:w-full after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 group-data-[orientation=vertical]:after:left-0 group-data-[orientation=vertical]:after:h-1 group-data-[orientation=vertical]:after:w-full group-data-[orientation=vertical]:after:translate-x-0 group-data-[orientation=vertical]:after:-translate-y-1/2 focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:outline-none group-data-[orientation=vertical]:[&>div]:rotate-90',
 			className
 		)}
 		{...props}
 	>
-		{/* TODO: Figure out how orientation works in v4 */}
-		{/* {withHandle && (
+		{withHandle && (
 			<div className="z-10 flex h-6 w-4 items-center justify-center rounded-sm bg-inherit md:h-4 md:w-3">
-				<GripVertical className="h-3 w-3 md:h-2.5 md:w-2.5" />
+				<PolarisDragHandleIcon className="h-3 w-3 md:h-2.5 md:w-2.5" />
 			</div>
-		)} */}
+		)}
 	</Separator>
 );
 
